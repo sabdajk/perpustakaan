698cf8abc2f5f855d88cee8e0054e2e207e5d1a1
Mohammad Jindan Dubbay Al Faresh <jindannajwa@gmail.com>
2024-07-17 12:19:12 +0700

    Update database

--- database/perpustakaan.sql
+++ database/perpustakaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Jul 2024 pada 16.03
+-- Waktu pembuatan: 17 Jul 2024 pada 07.18
 -- Versi server: 10.4.24-MariaDB
 -- Versi PHP: 8.1.6
 
@@ -67,22 +67,22 @@ CREATE TABLE `buku` (
 --
 
 INSERT INTO `buku` (`cover`, `id_buku`, `kategori`, `judul`, `pengarang`, `penerbit`, `tahun_terbit`, `jumlah_halaman`, `buku_deskripsi`, `popularity`) VALUES
-('654e505d7eda4.jpg', 'bis01', 'bisnis', 'Bussiness is fun', 'Coach yohannes g pauly', 'Rejove', '2016-11-10', 500, '7 strategi untuk membangun bisnis', 0),
-('654e62742ef40.jpg', 'bis02', 'bisnis', 'Digital Marketing Strategy', 'Simon kings north', '-', '2023-11-11', 250, 'Belajar strategi pemasaran digital', 0),
-('654e4dc4dc0c6.jpg', 'fil01', 'filsafat', 'Filosofi Teras', 'Henry Manampiring ', 'Kompas', '2018-11-26', 320, 'Filosofi Teras adalah sebuah buku pengantar filsafat Stoa yang dibuat khusus sebagai panduan moral anak muda. Buku ini ditulis untuk menjawab permasalahan tentang tingkat kekhawatiran yang cukup tinggi dalam skala nasional, terutama yang dialami oleh anak muda.', 0),
-('654e4f5e85f75.jpg', 'fil02', 'filsafat', 'Sejarah dunia yang disembunyikan ', 'Jonathan Black ', '-', '2007-11-10', 633, 'Banyak orang mengatakan bahwa sejarah ditulis oleh para pemenang. Hal ini sama sekali tak mengejutkan alias wajar belaka. Tetapi, bagaimana jika sejarahâ€”atau apa yang kita ketahui sebagai sejarahâ€”ditulis oleh orang yang salah? ', 6),
-('654e48e1a1680.jpg', 'inf01', 'informatika', 'Dasar dasar pemrogramman web', 'Sandhika Galih ', 'Inkara', '2023-10-18', 414, 'Website di era sekarang sudah menjadi kebutuhan utama yang tidak bisa diabaikan. Seluruh sektor bisnis atau edukasi dapat memanfaatkan website sebagai alat untuk promosi, tukar informasi, dan lainnya. Berdasarkan data dari World Wide Web Technology Surveys, dari seluruh website yang aktif, 88.2% menggunakan HTML dan 95.6% menggunakan CSS. Buku ini membahas tuntas mengenai HTML dan CSS sebagai fondasi dalam pembuatan website serta dilengkapi dengan Studi Kasus yang Relevan dan sesuai trend.', 1),
-('654e4a1c80441.jpg', 'inf02', 'informatika', 'Kursus Mandiri Python', 'Budi Raharjo', 'Informatika', '2022-05-10', 550, 'Belajar pemrogramman python dengan 5 tahapan yaitu : \r\n1. Dasar dasar python\r\n2. PBO(OOP)\r\n3. Eksplorasi Pustaka\r\n4. SQL &amp; MySql\r\n5. Pemrogramman GUI', 4),
-('654e4b44d4d0e.png', 'inf03', 'informatika', 'Pemrogramman Javascript Dan NodeJS untuk teknologi web', 'Budi Raharjo', 'Informatika', '2022-09-16', 500, 'Panduan membuat sistem aplikasi berbasis web dengan javascript dan nodeJs', 0),
-('654e4c1154bdd.jpg', 'inf04', 'informatika', 'Panduan Dasar ubuntu untuk pemula', 'Muhammad Ulil Fahri', 'Informatika', '2017-11-10', 404, 'Panduan awal ubuntu untuk pemula', 4),
-('654e4cd06e0de.jpeg', 'inf05', 'informatika', 'Belajar dasar Pemrogramman C++', 'Muhammad Taufik Dwi Putra', 'Informatika', '2018-11-10', 512, 'Panduan dasar belajar pemrogramman C++ untuk pemula', 1),
-('654e3d8b359df.jpg', 'nov01', 'novel', 'Dunia Sophie', 'Jostein Gardeer', 'Mizan', '1996-11-10', 800, 'Anda ingin tahu apa filsafat,  tetapi selalu tidak sempat,  terlalu kabur, abstrak, susah dan bertele tele?  Bacalah buku manis ini dimana sophie anak putri 14 tahun, menjadi terpesona karenanya. ', 0),
-('654e402a8ad79.jpg', 'nov02', 'novel', 'Perahu Kertas', 'Dewi Lestari', 'Bentang Pustaka', '2003-11-10', 444, 'Perahu Kertas bercerita tentang dua orang yang sama-sama unik bernama Kugy dan Keenan. ', 0),
-('654e4417e323e.jpeg', 'nov03', 'novel', 'Pulang', 'Tere Liye ', 'Sabak grip Nusantara ', '2015-11-10', 838, 'Pulangâ€ ini adalah novel yang menceritakan perjalanan hidup seorang anak laki-laki bernama Bujang, yang sejak berumur lima belas tahun di sebuah hutan rimba pedalaman Sumatera, rasa takutnya telah direnggut oleh seekor monster mengerikan (induk babi hutan).', 0),
-('654e456c2e275.jpg', 'nov04', 'novel', 'Surat Kecil Untuk Tuhan', 'Agnes Danovar', 'Inandra Publised', '2008-11-10', 200, 'Surat kecil untuk Tuhan adalah sebuah buku yang diangkat dari kisah nyata perjuangan gadis remaja bernama Gita Sesa Wanda Cantika alias Keke melawan kanker ganas.', 0),
-('654e46a08484e.jpg', 'nov05', 'novel', 'Ancika : dia yang bersamaku tahun 1995', 'Pidi baiq', 'Pastel books', '2021-09-02', 180, 'menceritakan tentang persahabatan antara Dilan dan Ancika Mehrunisa Rabu. Hubungan mereka yang semakin dekat membuat benih-benih cinta tumbuh dan hubungan mereka pun naik tingkat menjadi hubungan sepasang kekasih.', 0),
-('654e63b7841f5.jpg', 'sai01', 'sains', 'Cosmos', 'Karl sagan', '-', '2016-12-18', 488, 'Buku â€œKOSMOSâ€ adalah salah satu buku sains yang paling laris sepanjang sejarah. Dengan prosa jernih yang memukau, ahli astronomi Carl Sagan mengungkapkan alam semesta yang dihuni oleh suatu bentuk kehidupan yang baru saja mulai berpetualang menjelajahi luasnya antariksa.', 0),
-('654e64ee16c9a.jpg', 'sai02', 'sains', 'Kanker : Biografi suatu penyakit', 'Siddhartha mukherjee', '-', '2020-04-16', 682, 'kanker bukan hanya satu penyakit, melainkan banyak penyakit dengan ciri sama: pertumbuhan sel tak terkendali. Melawan kanker seolah melawan tubuh yang berkhianat: sel-sel kita sendiri yang berubah jadi ganas dan lepas kendali.', 0);
+('654e505d7eda4.jpg', 'bis01', 'bisnis', 'Bussiness is fun', 'Coach yohannes g pauly', 'Rejove', '2016-11-10', 500, '7 strategi untuk membangun bisnis', 1),
+('654e62742ef40.jpg', 'bis02', 'bisnis', 'Digital Marketing Strategy', 'Simon kings north', '-', '2023-11-11', 250, 'Belajar strategi pemasaran digital', 1),
+('654e4dc4dc0c6.jpg', 'fil01', 'filsafat', 'Filosofi Teras', 'Henry Manampiring ', 'Kompas', '2018-11-26', 320, 'Filosofi Teras adalah sebuah buku pengantar filsafat Stoa yang dibuat khusus sebagai panduan moral anak muda. Buku ini ditulis untuk menjawab permasalahan tentang tingkat kekhawatiran yang cukup tinggi dalam skala nasional, terutama yang dialami oleh anak muda.', 3),
+('654e4f5e85f75.jpg', 'fil02', 'filsafat', 'Sejarah dunia yang disembunyikan ', 'Jonathan Black ', '-', '2007-11-10', 633, 'Banyak orang mengatakan bahwa sejarah ditulis oleh para pemenang. Hal ini sama sekali tak mengejutkan alias wajar belaka. Tetapi, bagaimana jika sejarahâ€”atau apa yang kita ketahui sebagai sejarahâ€”ditulis oleh orang yang salah? ', 7),
+('654e48e1a1680.jpg', 'inf01', 'informatika', 'Dasar dasar pemrogramman web', 'Sandhika Galih ', 'Inkara', '2023-10-18', 414, 'Website di era sekarang sudah menjadi kebutuhan utama yang tidak bisa diabaikan. Seluruh sektor bisnis atau edukasi dapat memanfaatkan website sebagai alat untuk promosi, tukar informasi, dan lainnya. Berdasarkan data dari World Wide Web Technology Surveys, dari seluruh website yang aktif, 88.2% menggunakan HTML dan 95.6% menggunakan CSS. Buku ini membahas tuntas mengenai HTML dan CSS sebagai fondasi dalam pembuatan website serta dilengkapi dengan Studi Kasus yang Relevan dan sesuai trend.', 6),
+('654e4a1c80441.jpg', 'inf02', 'informatika', 'Kursus Mandiri Python', 'Budi Raharjo', 'Informatika', '2022-05-10', 550, 'Belajar pemrogramman python dengan 5 tahapan yaitu : \r\n1. Dasar dasar python\r\n2. PBO(OOP)\r\n3. Eksplorasi Pustaka\r\n4. SQL &amp; MySql\r\n5. Pemrogramman GUI', 6),
+('654e4b44d4d0e.png', 'inf03', 'informatika', 'Pemrogramman Javascript Dan NodeJS untuk teknologi web', 'Budi Raharjo', 'Informatika', '2022-09-16', 500, 'Panduan membuat sistem aplikasi berbasis web dengan javascript dan nodeJs', 4),
+('654e4c1154bdd.jpg', 'inf04', 'informatika', 'Panduan Dasar ubuntu untuk pemula', 'Muhammad Ulil Fahri', 'Informatika', '2017-11-10', 404, 'Panduan awal ubuntu untuk pemula', 6),
+('654e4cd06e0de.jpeg', 'inf05', 'informatika', 'Belajar dasar Pemrogramman C++', 'Muhammad Taufik Dwi Putra', 'Informatika', '2018-11-10', 512, 'Panduan dasar belajar pemrogramman C++ untuk pemula', 3),
+('654e3d8b359df.jpg', 'nov01', 'novel', 'Dunia Sophie', 'Jostein Gardeer', 'Mizan', '1996-11-10', 800, 'Anda ingin tahu apa filsafat,  tetapi selalu tidak sempat,  terlalu kabur, abstrak, susah dan bertele tele?  Bacalah buku manis ini dimana sophie anak putri 14 tahun, menjadi terpesona karenanya. ', 1),
+('654e402a8ad79.jpg', 'nov02', 'novel', 'Perahu Kertas', 'Dewi Lestari', 'Bentang Pustaka', '2003-11-10', 444, 'Perahu Kertas bercerita tentang dua orang yang sama-sama unik bernama Kugy dan Keenan. ', 1),
+('654e4417e323e.jpeg', 'nov03', 'novel', 'Pulang', 'Tere Liye ', 'Sabak grip Nusantara ', '2015-11-10', 838, 'Pulangâ€ ini adalah novel yang menceritakan perjalanan hidup seorang anak laki-laki bernama Bujang, yang sejak berumur lima belas tahun di sebuah hutan rimba pedalaman Sumatera, rasa takutnya telah direnggut oleh seekor monster mengerikan (induk babi hutan).', 1),
+('654e456c2e275.jpg', 'nov04', 'novel', 'Surat Kecil Untuk Tuhan', 'Agnes Danovar', 'Inandra Publised', '2008-11-10', 200, 'Surat kecil untuk Tuhan adalah sebuah buku yang diangkat dari kisah nyata perjuangan gadis remaja bernama Gita Sesa Wanda Cantika alias Keke melawan kanker ganas.', 1),
+('654e46a08484e.jpg', 'nov05', 'novel', 'Ancika : dia yang bersamaku tahun 1995', 'Pidi baiq', 'Pastel books', '2021-09-02', 180, 'menceritakan tentang persahabatan antara Dilan dan Ancika Mehrunisa Rabu. Hubungan mereka yang semakin dekat membuat benih-benih cinta tumbuh dan hubungan mereka pun naik tingkat menjadi hubungan sepasang kekasih.', 1),
+('654e63b7841f5.jpg', 'sai01', 'sains', 'Cosmos', 'Karl sagan', '-', '2016-12-18', 488, 'Buku â€œKOSMOSâ€ adalah salah satu buku sains yang paling laris sepanjang sejarah. Dengan prosa jernih yang memukau, ahli astronomi Carl Sagan mengungkapkan alam semesta yang dihuni oleh suatu bentuk kehidupan yang baru saja mulai berpetualang menjelajahi luasnya antariksa.', 1),
+('654e64ee16c9a.jpg', 'sai02', 'sains', 'Kanker : Biografi suatu penyakit', 'Siddhartha mukherjee', '-', '2020-04-16', 682, 'kanker bukan hanya satu penyakit, melainkan banyak penyakit dengan ciri sama: pertumbuhan sel tak terkendali. Melawan kanker seolah melawan tubuh yang berkhianat: sel-sel kita sendiri yang berubah jadi ganas dan lepas kendali.', 1);
 
 -- --------------------------------------------------------
 
@@ -128,9 +128,8 @@ CREATE TABLE `member` (
 --
 
 INSERT INTO `member` (`nisn`, `kode_member`, `nama`, `password`, `jenis_kelamin`, `kelas`, `jurusan`, `no_tlp`, `tgl_pendaftaran`) VALUES
-(110000123, '321', 'noah', '$2y$10$o.XuMw9FhWd87awGau0c7ujQo7gRD2WDGPwSV6YOuE05l20UgCjoK', 'Laki laki', 'X', 'Teknik Konstruksi Perumahan', '0858748374834', '2024-07-13'),
-(1200001222, '123', 'kaslana', '$2y$10$B0SKoFI9LREWtAfgkykdeuzI6Xo3Iycb0l0VTVMJZpP/ZeK9fI1T.', 'Perempuan', 'XII', 'Sistem Informatika Jaringan dan Aplikasi', '089734547463', '2024-07-13'),
-(1300001200, '1122', 'sofia', '$2y$10$0OfmQdmmr4uIjFCgzdUhDelLiNQsFly7D782h2WvK5jiicll1xyTu', 'Perempuan', 'XI', 'Rekayasa Perangkat Lunak', '085347834783', '2024-07-13');
+(1200001211, '213', 'daniel', '$2y$10$YJbSjdaPVD4yF6QXTMK9yestBjhV.rqM7Ijbt9Q2koQlZAxQ.IY.K', 'Laki laki', 'XI', 'Teknik Konstruksi Perumahan', '089637647364', '2024-07-17'),
+(1200001222, '123', 'kaslana', '$2y$10$B0SKoFI9LREWtAfgkykdeuzI6Xo3Iycb0l0VTVMJZpP/ZeK9fI1T.', 'Perempuan', 'XII', 'Sistem Informatika Jaringan dan Aplikasi', '089734547463', '2024-07-13');
 
 -- --------------------------------------------------------
 
@@ -146,13 +145,6 @@ CREATE TABLE `peminjaman` (
   `tgl_peminjaman` date NOT NULL,
   `tgl_pengembalian` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `peminjaman`
---
-
-INSERT INTO `peminjaman` (`id_peminjaman`, `id_buku`, `nisn`, `id_admin`, `tgl_peminjaman`, `tgl_pengembalian`) VALUES
-(105, 'fil01', 1200001222, 1, '2024-07-08', '2024-07-15');
 
 -- --------------------------------------------------------
 
@@ -170,17 +162,6 @@ CREATE TABLE `pengembalian` (
   `keterlambatan` enum('YA','TIDAK') NOT NULL,
   `denda` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `pengembalian`
---
-
-INSERT INTO `pengembalian` (`id_pengembalian`, `id_peminjaman`, `id_buku`, `nisn`, `id_admin`, `buku_kembali`, `keterlambatan`, `denda`) VALUES
-(77, 101, 'fil02', 1200001222, 1, '2024-07-13', 'TIDAK', 0),
-(78, 102, 'inf02', 1200001222, 1, '2024-07-13', 'TIDAK', 0),
-(79, 102, 'inf02', 1200001222, 1, '2024-07-13', 'TIDAK', 0),
-(80, 103, 'fil02', 1200001222, 2, '2024-07-13', 'TIDAK', 0),
-(81, 104, 'fil01', 1200001222, 1, '2024-07-13', 'TIDAK', 0);
 
 -- --------------------------------------------------------
 
@@ -201,9 +182,8 @@ CREATE TABLE `subscriptions` (
 --
 
 INSERT INTO `subscriptions` (`id`, `endpoint`, `keys_auth`, `keys_p256dh`, `created_at`) VALUES
-(1, 'https://fcm.googleapis.com/fcm/send/fa6AphOG1Cs:APA91bGmCjnrhj6FgiVmEdUPbpXPA4mNpJkz-3LXKbOvZBbxZSLFmqjGVHs6v8lFWW9iYY8snBSXVA6R5HGZi0Y6vRZLq_Ss51wrVGZX5SUwkW3ynjCR3DjfWi1tqb1N2Faikbkp5PQa', 'o1U_9N5jjWqMGGw88uLt0Q', 'BPFzkXn8r-HaFZ3QnW-XZLhfRL2VP_CdEZmtMjTUKKQf145cBDOSPq1zp5KWeRUkJIlVp3EK6hBE882zhX9pPYQ', '2024-07-12 23:28:29'),
-(2, 'https://fcm.googleapis.com/fcm/send/cUv1DEhVREQ:APA91bERndG_Z2iR6BfBaqeplhkb_A2ZCVnWMwacQ3EfJVB7LVxO3DPGjsqjMhaly-mlmScR67Y-cPBaL5hzKi_EuwsmNndUSpfemM5Irm8iBx-eIhwqqx-_trtRsuCBUt_FeBdWhoOY', 'mZErGmAhBSWGIFuwS2NyoQ', 'BKjvr7HOjC-iLTe_5ZyqdlI2rnKMetqoLXIO3n78UAWJ0Ajjp6Ze_bFM1IPnmTHQx-B2KkqWEj1-ddRAijmgxIQ', '2024-07-12 20:50:54'),
-(3, 'https://fcm.googleapis.com/fcm/send/dRXEojqMKMU:APA91bGjc1HmbBEX_ZGtKcZpH3kyHGOg515wXVdxq4_09hFZCw4c9qUraHVF5hy1jXGMPYKVRCbsavexCoiRYmrCeqZhIdQaV0LrYfnn-qfZswH5mdDEML9k3K1mztcej96ztYq_FvLS', '3YIoifNYl3nV3HBKUflPHw', 'BPBzakuNYMZdA3eSXyTEpiTyme7-zFXVamsN28OsvXsZy6LdgRT5A2RCEAMiXcCZtwZOGDM1LY7kBNd-fV6yVPE', '2024-07-13 04:23:44');
+(4, 'https://fcm.googleapis.com/fcm/send/dRXEojqMKMU:APA91bGjc1HmbBEX_ZGtKcZpH3kyHGOg515wXVdxq4_09hFZCw4c9qUraHVF5hy1jXGMPYKVRCbsavexCoiRYmrCeqZhIdQaV0LrYfnn-qfZswH5mdDEML9k3K1mztcej96ztYq_FvLS', '3YIoifNYl3nV3HBKUflPHw', 'BPBzakuNYMZdA3eSXyTEpiTyme7-zFXVamsN28OsvXsZy6LdgRT5A2RCEAMiXcCZtwZOGDM1LY7kBNd-fV6yVPE', '2024-07-15 12:01:03'),
+(5, 'https://fcm.googleapis.com/fcm/send/dRP5BzMSVNo:APA91bHY5PFHd3fEl_HRkmfcQGQTmJE2wjF8ncuFeMQX_70YzBV0ZCUtYn_eu4w-9ybcSPtvQ-e3KLz3yrDjUZWvJQuLxjyuMvWBvpTDXvjR8IlAR0BgX1kZW_JN6kkimoYzBJjLaqNd', 'P7E1hKV-jLPhaiQgADzvng', 'BHb0eZBQaPBYGq5FdzRjheejWkeNWTQDuL2VnZrB_uOsHbL89gvdXbad4DYLchKdHNHodWYSCPsAokGnahqNFA0', '2024-07-15 15:22:26');
 
 --
 -- Indexes for dumped tables
@@ -275,19 +255,19 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT untuk tabel `peminjaman`
 --
 ALTER TABLE `peminjaman`
-  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=186;
 
 --
 -- AUTO_INCREMENT untuk tabel `pengembalian`
 --
 ALTER TABLE `pengembalian`
-  MODIFY `id_pengembalian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+  MODIFY `id_pengembalian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=127;
 
 --
 -- AUTO_INCREMENT untuk tabel `subscriptions`
 --
 ALTER TABLE `subscriptions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
